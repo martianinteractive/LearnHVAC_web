6b8a21d075a6ffea50955bca3adf7acaa2a771c0
jpemberthy <jpemberthy@gmail.com>
2009-12-09 17:27:42 -0500

    no cron jobs here yet.

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,9 +32,4 @@ namespace(:deploy) do
     run "touch #{release_path}/tmp/restart.txt"
   end
   
-  desc "Update the crontab file"
-  task :update_crontab, :roles => :db do
-    run "cd #{release_path} && whenever --update-crontab #{application}"
-  end
-
 end
