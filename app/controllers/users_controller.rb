e506435fb25205bb4a38621364b48feda729c194
jpemberthy <jpemberthy@gmail.com>
2010-03-02 18:48:39 -0500

    Converting html files to haml files.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,40 +2,21 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @users }
-    end
   end
 
-  # GET /users/1
-  # GET /users/1.xml
   def show
     @user = User.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @user }
-    end
   end
 
   def new
     @user = User.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @user }
-    end
   end
 
-  # GET /users/1/edit
   def edit
     @user = User.find(params[:id])
   end
 
-  # POST /users
-  # POST /users.xml
+
   def create
     @user = User.new(params[:user])
 
@@ -50,8 +31,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # PUT /users/1
-  # PUT /users/1.xml
   def update
     @user = User.find(params[:id])
 
@@ -66,8 +45,7 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.xml
+
   def destroy
     @user = User.find(params[:id])
     @user.destroy
