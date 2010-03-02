02cda1c8d1c01e817c6a866acc917044f586ccd5
jpemberthy <jpemberthy@gmail.com>
2010-03-02 11:24:33 -0500

    Haml convertion fixes. Added users_sessions#new view.

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Learnhvac::Application.routes.draw do |map|
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "user_sessions#new"
 
   # See how all your routes lay out with "rake routes"
 
