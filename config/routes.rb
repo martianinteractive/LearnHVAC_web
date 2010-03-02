d7e6545423aaf90dbc54f095a5b24f1302cab916
jpemberthy <jpemberthy@gmail.com>
2010-03-02 16:32:11 -0500

    routes for login & logout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Learnhvac::Application.routes.draw do |map|
+  
   resources :users
   resources :user_sessions
+  
+  match 'login'   => 'user_sessions#new'
+  match 'logout'  => 'user_sessions#destroy'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -51,7 +55,7 @@ Learnhvac::Application.routes.draw do |map|
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "user_sessions#new"
+  # root :to => "user_sessions#new"
 
   # See how all your routes lay out with "rake routes"
 
