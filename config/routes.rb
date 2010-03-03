f2a3a42ce601066fab7c52714da1ba09089e8413
jpemberthy <jpemberthy@gmail.com>
2010-03-03 12:02:19 -0500

    temporary setting users as root.

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ Learnhvac::Application.routes.draw do |map|
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "user_sessions#new"
+  root :to => "users#index"
 
   # See how all your routes lay out with "rake routes"
 
