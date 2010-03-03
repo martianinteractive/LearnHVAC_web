db27d1e3facc271a884f3f36f36f1a2a621fc754
jpemberthy <jpemberthy@gmail.com>
2010-03-03 09:41:50 -0500

    Fixing Users#index haml convertion.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   ROLES = { :guest => 0, :student => 1, :instructor => 2, :administrator => 3, :superadmin => 4 }
   acts_as_authentic
+  
+  def role
+    ROLES.index(read_attribute(:role_code))
+  end
 end
