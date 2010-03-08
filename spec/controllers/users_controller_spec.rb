a16d218bab19fab8082efa8fa407181726d51951
jpemberthy <jpemberthy@gmail.com>
2010-03-08 14:21:28 -0500

    users_controller_spec.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,11 @@
 require File.dirname(__FILE__) + "/../spec_helper"
 
 describe UsersController do
+  
+  before(:each) do
+    @admin = Factory(:user, :login => "joedoe", :email => "jdoe@lhvac.com")
+    login_as(@admin)
+  end
     
   describe "GET index" do
     it "assigns all users as @users" do
@@ -37,89 +42,78 @@ describe UsersController do
     end
   end
   
-  # describe "POST create" do
-  # 
-  #   describe "with valid params" do
-  #     it "assigns a newly created user as @user" do
-  #       User.stub(:new).with({'these' => 'params'}) { mock_user(:save => true) }
-  #       post :create, :user => {'these' => 'params'}
-  #       assigns(:user).should be(mock_user)
-  #     end
-  # 
-  #     it "redirects to the created user" do
-  #       User.stub(:new) { mock_user(:save => true) }
-  #       post :create, :user => {}
-  #       response.should redirect_to(user_url(mock_user))
-  #     end
-  #   end
-  # 
-  #   describe "with invalid params" do
-  #     it "assigns a newly created but unsaved user as @user" do
-  #       User.stub(:new).with({'these' => 'params'}) { mock_user(:save => false) }
-  #       post :create, :user => {'these' => 'params'}
-  #       assigns(:user).should be(mock_user)
-  #     end
-  # 
-  #     it "re-renders the 'new' template" do
-  #       User.stub(:new) { mock_user(:save => false) }
-  #       post :create, :user => {}
-  #       response.should render_template('new')
-  #     end
-  #   end
-  # 
-  # end
-  # 
-  # describe "PUT update" do
-  # 
-  #   describe "with valid params" do
-  #     it "updates the requested user" do
-  #       User.should_receive(:find).with("37") { mock_user }
-  #       mock_user.should_receive(:update_attributes).with({'these' => 'params'})
-  #       put :update, :id => "37", :user => {'these' => 'params'}
-  #     end
-  # 
-  #     it "assigns the requested user as @user" do
-  #       User.stub(:find) { mock_user(:update_attributes => true) }
-  #       put :update, :id => "1"
-  #       assigns(:user).should be(mock_user)
-  #     end
-  # 
-  #     it "redirects to the user" do
-  #       User.stub(:find) { mock_user(:update_attributes => true) }
-  #       put :update, :id => "1"
-  #       response.should redirect_to(user_url(mock_user))
-  #     end
-  #   end
-  # 
-  #   describe "with invalid params" do
-  #     it "assigns the user as @user" do
-  #       User.stub(:find) { mock_user(:update_attributes => false) }
-  #       put :update, :id => "1"
-  #       assigns(:user).should be(mock_user)
-  #     end
-  # 
-  #     it "re-renders the 'edit' template" do
-  #       User.stub(:find) { mock_user(:update_attributes => false) }
-  #       put :update, :id => "1"
-  #       response.should render_template('edit')
-  #     end
-  #   end
-  # 
-  # end
-  # 
-  # describe "DELETE destroy" do
-  #   it "destroys the requested user" do
-  #     User.should_receive(:find).with("37") { mock_user }
-  #     mock_user.should_receive(:destroy)
-  #     delete :destroy, :id => "37"
-  #   end
-  # 
-  #   it "redirects to the users list" do
-  #     User.stub(:find) { mock_user(:destroy => true) }
-  #     delete :destroy, :id => "1"
-  #     response.should redirect_to(users_url)
-  #   end
-  # end
+  describe "POST create" do
+  
+    describe "with valid params" do
+      it "assigns a newly created user as @user" do
+        proc{ post :create, :user => Factory.attributes_for(:user) }.should change(User, :count).by(1)
+        assigns(:user).should be_instance_of(User)
+      end
+  
+      it "redirects to the created user" do
+        post :create, :user => Factory.attributes_for(:user)
+        response.should redirect_to(user_url(assigns(:user)))
+      end
+    end
+  
+    describe "with invalid params" do
+      it "assigns a newly created but unsaved user as @user" do
+        proc{ post :create, :user => Factory.attributes_for(:user).merge(:first_name => "Jame$") }.should_not change(User, :count)
+        assigns(:user).should be_instance_of(User)
+      end
+      
+      it "re-renders the 'new' template" do
+        post :create, :user => Factory.attributes_for(:user).merge(:first_name => "Jame$")
+        response.should render_template(:new)
+      end
+    end
+  
+  end
+  
+  describe "PUT update" do
+    
+    before(:each) do
+      @user = Factory(:user)
+    end
+    
+    describe "with valid params" do      
+      it "updates the requested user" do
+        put :update, :id => @user.id, :user => { :first_name => "Joe" }
+        assigns(:user).should == @user.reload
+        @user.first_name.should == "Joe"
+      end
+      
+      it "redirects to the user" do
+        put :update, :id => @user.id, :user => { :last_name => "Doex" }
+        response.should redirect_to(user_url(@user))
+      end
+    end
+      
+    describe "with invalid params" do  
+      it "not update the user" do
+        put :update, :id => @user.id, :user => { :first_name => "Jame$", :last_name => "" }
+        assigns(:user).should_not equal(@user.reload)
+        
+        assigns(:user).should be_instance_of(User)
+      end
+      
+      it "re-renders the 'edit' template" do
+        put :update, :id => @user.id, :user => { :first_name => "Jame$", :last_name => "" }
+        response.should render_template('edit')
+      end
+    end
+  end
+  
+  describe "DELETE destroy" do
+    before(:each) do
+      @user = Factory(:user)
+    end
+    
+    it "destroys the requested user" do
+      proc { delete :destroy, :id => @user.id }.should change(User, :count).by(-1)
+      response.should redirect_to(users_path)
+    end
+  end
   
   def mock_user(attrs = {})
     @mock_user ||= Factory(:user, attrs)
