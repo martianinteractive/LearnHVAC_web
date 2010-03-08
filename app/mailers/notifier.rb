29e2fe60d786a95a5b698aee5893568319f54c6b
jpemberthy <jpemberthy@gmail.com>
2010-03-08 11:50:34 -0500

    Using the new ActionMailer API.

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,13 +1,10 @@
 class Notifier < ActionMailer::Base
+  ActionMailer::Base.default_url_options[:host] = "lhvac.com"
   default :from => "from@lhvac.com"
-  # default :host => "lhvac.com" #doesn't seem to work like default :from
-  default_url_options[:host] = "lhvac.com"
-  
-  def password_reset_instructions(user)  
-    subject       "Password Reset Instructions"  
-    recipients    user.email  
-    sent_on       Time.now  
-    body          :edit_password_reset_url => edit_password_reset_url(user.perishable_token)  
+    
+  def password_reset_instructions(user) 
+    @user = user
+    mail(:to => user.email, :subject => "Password Reset Instructions")
   end
   
 end
