a8f93ba06ebf2dc3e78e8c165201ba6817198465
jpemberthy <jpemberthy@gmail.com>
2010-03-08 17:13:38 -0500

    Enabling jQuery.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < ActionController::Base
     if current_user
       store_location
       flash[:notice] = "You must be logged out to access this page"
-      redirect_to account_url
+      redirect_to root_path
       return false
     end
   end
