73c2a78fe8e5ce41cb9714a72ceaad4ad3dbbe28
jpemberthy <jpemberthy@gmail.com>
2010-03-15 12:46:10 -0500

    from accounts controller we want to save without session maintenance since the user has not yet been activated.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -6,11 +6,15 @@ class AccountsController < ApplicationController
     @account = User.new
   end
   
+  # Saving without session maintenance to skip
+  # auto-login which can't happen here because
+  # the User has not yet been activated
   def create
     @account = User.new(params[:user])
+    @account.active = false
     
-    if @user.save
-      #password confirmation
+    if @account.save_without_session_maintenance
+      @account.deliver_activation_instructions!
       flash[:notice] = "Your account has been created. Please check your e-mail for your account activation instructions!"
       redirect_to root_path
     else
