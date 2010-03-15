25ff622cfdff768cc12f503638797e504bb99498
jpemberthy <jpemberthy@gmail.com>
2010-03-15 11:38:24 -0500

    post to accounts instead of users.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -7,7 +7,15 @@ class AccountsController < ApplicationController
   end
   
   def create
+    @account = User.new(params[:user])
     
+    if @user.save
+      #password confirmation
+      flash[:notice] = "Your account has been created. Please check your e-mail for your account activation instructions!"
+      redirect_to root_path
+    else
+      render :action => :new
+    end
   end
   
 end
