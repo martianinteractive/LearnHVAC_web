f5ca134c5a4464f03ca9b041c58e13b23173d836
jpemberthy <jpemberthy@gmail.com>
2010-03-16 16:12:25 -0500

    global_system_controller rest of CRUD and specs.

--- app/controllers/global_system_variables_controller.rb
+++ app/controllers/global_system_variables_controller.rb
@@ -12,11 +12,6 @@ class GlobalSystemVariablesController < ApplicationController
 
   def new
     @global_system_variable = GlobalSystemVariable.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @global_system_variable }
-    end
   end
 
   def edit
@@ -26,28 +21,20 @@ class GlobalSystemVariablesController < ApplicationController
   def create
     @global_system_variable = GlobalSystemVariable.new(params[:global_system_variable])
 
-    respond_to do |format|
-      if @global_system_variable.save
-        format.html { redirect_to(@global_system_variable, :notice => 'GlobalSystemVariable was successfully created.') }
-        format.xml  { render :xml => @global_system_variable, :status => :created, :location => @global_system_variable }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @global_system_variable.errors, :status => :unprocessable_entity }
-      end
+    if @global_system_variable.save
+      redirect_to(@global_system_variable, :notice => 'GlobalSystemVariable was successfully created.')
+    else
+      render :action => :new
     end
   end
 
   def update
     @global_system_variable = GlobalSystemVariable.find(params[:id])
 
-    respond_to do |format|
-      if @global_system_variable.update_attributes(params[:global_system_variable])
-        format.html { redirect_to(@global_system_variable, :notice => 'GlobalSystemVariable was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @global_system_variable.errors, :status => :unprocessable_entity }
-      end
+    if @global_system_variable.update_attributes(params[:global_system_variable])
+      redirect_to(@global_system_variable, :notice => 'GlobalSystemVariable was successfully updated.')
+    else
+      render :action => :edit
     end
   end
 
@@ -55,9 +42,6 @@ class GlobalSystemVariablesController < ApplicationController
     @global_system_variable = GlobalSystemVariable.find(params[:id])
     @global_system_variable.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(global_system_variables_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(global_system_variables_url)
   end
 end
--- spec/controllers/global_system_variables_controller_spec.rb
+++ spec/controllers/global_system_variables_controller_spec.rb
@@ -27,6 +27,79 @@ describe GlobalSystemVariablesController do
     end
   end
   
+  describe "GET new" do
+    it "" do
+      get :new
+      response.should render_template(:new)
+      assigns(:global_system_variable).should be_instance_of(GlobalSystemVariable)
+    end
+  end
+  
+  describe "GET edit" do
+    it "" do
+      GlobalSystemVariable.expects(:find).with("37").returns(mock_global_var)
+      get :edit, :id => "37"
+      response.should render_template(:edit)
+      assigns(:global_system_variable).should be(mock_global_var)
+    end
+  end
+  
+  describe "POST create" do
+    describe "with valid params" do
+      it "should change the GlobalSystemVariable count" do
+        proc{ post :create, :global_system_factory => Factory.attributes_for(:global_system_variable) }.should change(GlobalSystemVariable, :count).by(1)
+      end
+  
+      it "redirects to the created global_system_variable" do
+        post :create, :global_system_variable => Factory.attributes_for(:global_system_variable)
+        response.should redirect_to(global_system_variable_path(assigns(:global_system_variable)))
+      end
+    end
+  
+    pending "Define invalid attrs for global system var"
+    describe "with invalid params" do
+    end
+  end
+  
+  describe "PUT update" do
+    
+    before(:each) do
+      @global_system_variable = Factory(:global_system_variable)
+    end
+    
+    describe "with valid params" do      
+      it "updates the requested global_system_variable" do
+        put :update, :id => @global_system_variable.id, :global_system_variable => { :name => "Cold var" }
+        @global_system_variable.reload["name"].should == "Cold var"
+      end
+      
+      it "redirects to the global_system_variable" do
+        put :update, :id => @global_system_variable.id, :global_system_variable => { :name => "Cold var" }
+        response.should redirect_to(global_system_variable_path(@global_system_variable.id))
+      end
+    end
+    
+    pending "Define invalid attrs for global system var"
+    describe "with invalid params" do  
+    end
+  end
+  
+  
+  describe "DELETE destroy" do
+    before(:each) do
+      @global_system_variable = Factory(:global_system_variable)
+    end
+    
+    it "destroys the requested global_system_variable" do
+      proc { delete :destroy, :id => @global_system_variable.id }.should change(GlobalSystemVariable, :count).by(-1)
+    end
+  
+    it "redirects to the global_system_variables list" do
+      delete :destroy, :id => @global_system_variable.id
+      response.should redirect_to(global_system_variables_path)
+    end
+  end
+  
   describe "Authorization" do
     it "should require an authenticated superadmin for all actions" do
       @admin.role_code = User::ROLES[:student]
