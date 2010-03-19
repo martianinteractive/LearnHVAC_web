f8ece83845fa28a6cd3155ba4e0e35d3f85c27f8
jpemberthy <jpemberthy@gmail.com>
2010-03-19 14:15:00 -0500

    admin/scenario_variables full & specs.

--- app/controllers/admin/scenario_variables_controller.rb
+++ app/controllers/admin/scenario_variables_controller.rb
@@ -1,2 +1,56 @@
 class Admin::ScenarioVariablesController < ApplicationController
+  before_filter :require_admin
+  before_filter :find_scenario
+  before_filter :find_scenario_variable, :only => [:show, :edit, :update]
+  layout "admin"
+  
+  def index
+    @scenario_variables = @scenario.scenario_variables
+  end
+  
+  def new
+    @scenario_variable = ScenarioVariable.new
+  end
+  
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+    @scenario_variable = ScenarioVariable.new(params[:scenario_variable])
+    @scenario_variable.scenario = @scenario
+    
+    if @scenario_variable.save
+      redirect_to(admin_scenario_scenario_variable_path(@scenario, @scenario_variable), :notice => 'ScenarioVariable was successfully created.')
+    else
+      render :action => "new"
+    end
+  end
+
+  def update
+    if @scenario_variable.update_attributes(params[:scenario_variable])
+      redirect_to(admin_scenario_scenario_variable_path(@scenario, @scenario_variable), :notice => 'ScenarioVariable was successfully updated.')
+    else
+      render :action => "edit"
+    end
+  end
+
+  def destroy
+    @scenario.scenario_variables.find(params[:id]).destroy
+    redirect_to(admin_scenario_scenario_variables_path(@scenario))
+  end
+  
+  
+  private
+  
+  def find_scenario
+    @scenario = Scenario.find(params[:scenario_id])
+  end
+  
+  def find_scenario_variable
+    @scenario_variable = @scenario.scenario_variables.find(params[:id])
+  end
+  
 end
