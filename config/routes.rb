b2e885fd4cf16b33ed8b1d084e13fac08a073ff8
jpemberthy <jpemberthy@gmail.com>
2010-03-19 13:49:46 -0500

    cleaning routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Learnhvac::Application.routes.draw do |map|
-  
   resources :system_variables
   resources :accounts
   resources :users
@@ -11,76 +10,20 @@ Learnhvac::Application.routes.draw do |map|
   match 'sign_up' => 'accounts#new', :as => "sign_up"
   match 'register/:activation_code' => 'activations#new', :as => "register"
   match 'activate/:id' => 'activations#create', :as => "activate"
-
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
   
   resources :scenarios do
-    member do
-      get :variables_edit
-    end
     resources :scenario_variables
   end
 
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get :short
-  #       post :toggle
-  #     end
-  #
-  #     collection do
-  #       get :sold
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get :recent, :on => :collection
-  #     end
-  #   end
-  
   match 'admin/dashboard' => 'admin/dashboard#show', :as => "admin_dashboard"
   
   namespace :admin do
     resources :system_variables
     resources :institutions
-    resources :scenarios
+    resources :scenarios do
+      resources :scenario_variables
+    end
   end
 
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
   root :to => "user_sessions#new"
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
 end
