07d49bbc5078161a9b3d907de8f797dd07e6715b
jpemberthy <jpemberthy@gmail.com>
2010-03-24 16:37:35 -0500

    scenario_fields module.

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -2,31 +2,13 @@ class Scenario
   include Mongoid::Document
   include Mongoid::Timestamps
   include Mongoid::Document::ProtectedAttributes
-  
-  field :scenario_id
-  field :name
-  field :movie_url
-  field :thumbnail_url
-  field :short_description
-  field :description
-  field :goal
-  field :level,                           :type => Integer, :default => 1
-  field :inputs_visible,                  :type => Boolean, :default => true
-  field :inputs_enabled,                  :type => Boolean, :default => true
-  field :faults_visible,                  :type => Boolean, :default => true
-  field :faults_enabled,                  :type => Boolean, :default => true
-  field :valve_info_enabled,              :type => Boolean, :default => true
-  field :allow_longterm_date_change,      :type => Boolean, :default => false
-  field :allow_realtime_datetime_change,  :type => Boolean, :default => false
-  field :longterm_start_date,             :type => Date
-  field :longterm_stop_date,              :type => Date
-  field :realtime_start_datetime,         :type => DateTime  
-    
-  named_scope :recently_created, criteria.where(:created_at.gt => (Time.now + 30.days))
-  named_scope :recently_updated, criteria.where(:updated_at.gt => (Time.now + 30.days))
+  include ScenarioFields
   
   has_many :scenario_variables
   belongs_to_related :user
+  
+  named_scope :recently_created, criteria.where(:created_at.gt => (Time.now + 30.days))
+  named_scope :recently_updated, criteria.where(:updated_at.gt => (Time.now + 30.days))
   
   accepts_nested_attributes_for :scenario_variables
   
---
+++ lib/scenario_fields.rb
@@ -0,0 +1,22 @@
+module ScenarioFields  
+  def self.included(parent)
+    parent.field :scenario_id
+    parent.field :name
+    parent.field :movie_url
+    parent.field :thumbnail_url
+    parent.field :short_description
+    parent.field :description
+    parent.field :goal
+    parent.field :level,                           :type => Integer, :default => 1
+    parent.field :inputs_visible,                  :type => Boolean, :default => true
+    parent.field :inputs_enabled,                  :type => Boolean, :default => true
+    parent.field :faults_visible,                  :type => Boolean, :default => true
+    parent.field :faults_enabled,                  :type => Boolean, :default => true
+    parent.field :valve_info_enabled,              :type => Boolean, :default => true
+    parent.field :allow_longterm_date_change,      :type => Boolean, :default => false
+    parent.field :allow_realtime_datetime_change,  :type => Boolean, :default => false
+    parent.field :longterm_start_date,             :type => Date
+    parent.field :longterm_stop_date,              :type => Date
+    parent.field :realtime_start_datetime,         :type => DateTime
+  end  
+end
