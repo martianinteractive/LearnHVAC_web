bc5c8c1c0f96daf304da3a8ba505245b135e8615
jpemberthy <jpemberthy@gmail.com>
2010-03-25 14:12:07 -0500

    membership validations.

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,7 +2,10 @@ class Membership < ActiveRecord::Base
   belongs_to :group
   belongs_to :student, :class_name => "User", :foreign_key => "student_id"
   
-  validate :group_id, :presence => true
-  validate :student_id, :presence => true
-  validate :group_id, :uniqueness => true, :scope => :student_id
+  validates :group, :presence => true
+  validates :student, :presence => true
+  
+  # Passing :scope to the new validates method 
+  # "validates :student_id, :uniqueness => true" isn't working. 
+  validates_uniqueness_of :student_id, :scope => :group_id 
 end
--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -1,5 +1,33 @@
-require 'spec_helper'
+require File.dirname(__FILE__) + "/../spec_helper"
 
 describe Membership do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @student    = user_with_role(:student)
+    @group      = Factory(:group, :instructor => user_with_role(:instructor))
+    @membership = Factory.build(:membership, :group => @group, :student => @student)
+  end
+  
+  it "" do
+    @membership.should be_valid
+  end
+  
+  it "should not be valid without group" do
+    @membership.group = nil
+    @membership.should_not be_valid
+    @membership.errors[:group].should_not be_empty
+  end
+  
+  it "should not be valid without student" do
+    @membership.student = nil
+    @membership.should_not be_valid
+    @membership.errors[:student].should_not be_empty
+  end
+  
+  it "should validates uniqueness of group_student" do
+    @membership.save
+    membership = Factory.build(:membership, :group => @group, :student => @student)
+    membership.should_not be_valid
+    membership.errors[:student_id].should_not be_empty
+  end
+  
 end
