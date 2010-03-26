8007fde8ff15b51ff2a09c051b74f687f406137a
jpemberthy <jpemberthy@gmail.com>
2010-03-26 12:55:04 -0500

    master_scenario.clone!

--- app/models/master_scenario.rb
+++ app/models/master_scenario.rb
@@ -11,4 +11,19 @@ class MasterScenario
   
   validates_presence_of :name, :user
   
+  
+  def clone!
+    clon_atts = self.attributes
+    clon_atts.delete("_id")
+    clon_atts.delete("system_variables")
+    clon_atts["name"] = "#{self.name}_clon"
+    clon = MasterScenario.create(clon_atts)
+    self.system_variables.each do |sv| 
+      sys_var_clon_atts = sv.attributes
+      sys_var_clon_atts.delete("_id")
+      clon.system_variables.create(sys_var_clon_atts)
+    end
+    clon
+  end
+  
 end
--- spec/models/master_scenario_spec.rb
+++ spec/models/master_scenario_spec.rb
@@ -19,6 +19,29 @@ describe MasterScenario do
       @master_scenario = Factory.build(:master_scenario, :name => "m scenario", :user => @admin)
       @master_scenario.should be_valid
     end
+  end
+    
+  context "Cloning" do
+    
+    before(:each) do
+      Factory(:system_variable, :master_scenario => @master_scenario)
+      Factory(:system_variable, :master_scenario => @master_scenario)
+    end
+    
+    it "" do
+      proc { @master_scenario.clone! }.should change(MasterScenario, :count).by(1)
+    end
+    
+    it "should also clone the system variables" do
+      ms_clon = @master_scenario.clone!
+      ms_clon.system_variables.should have(@master_scenario.system_variables.count).variables
+    end
+    
+    it "should name the cloned scenario as original_name_clon" do
+      ms_clon = @master_scenario.clone!
+      ms_clon.name.should == "#{@master_scenario.name}_clon"
+    end
     
   end
+      
 end
