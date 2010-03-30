9e847fb5c81bca0d07ab5f9311eb3e09e03b58b7
jpemberthy <jpemberthy@gmail.com>
2010-03-30 10:56:24 -0500

    Basic tagging system for mongoid documents, tags acts as scenario array. Need text_fiedl with autocomplete.

--- app/models/master_scenario.rb
+++ app/models/master_scenario.rb
@@ -1,16 +1,16 @@
 class MasterScenario
   include Mongoid::Document
   include Mongoid::Timestamps
-  
   field :name
   field :description
+  field :tags, :type => Array
 
   embed_many :system_variables
+  
   has_many_related :scenarios
   belongs_to_related :user
   
   validates_presence_of :name, :user
-  
   
   def clone!
     clon_atts = self.attributes
@@ -24,6 +24,26 @@ class MasterScenario
       clon.system_variables.create(sys_var_clon_atts)
     end
     clon
+  end
+  
+  
+  # Move this to a module.
+  
+  def tag_list=(tags)
+    self.tags = tags.split(",").collect{|t| t.strip}
+  end
+  
+  def tag_list
+    self.tags.join(", ") if tags
+  end
+  
+  def self.tags
+    self.all.collect{ |ms| ms.tags }.flatten.uniq.compact
+  end
+  
+  def self.tagged_with(_tags)
+    _tags = [_tags] unless _tags.is_a? Array
+    criteria.in(:tags => _tags)
   end
   
 end
