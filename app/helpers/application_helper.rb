57169ecbd6df844d5c557a24b5da6840e9ebb2d3
jpemberthy <jpemberthy@gmail.com>
2010-03-31 17:32:33 -0500

    js behavior for adding multiple scenarios.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,18 @@ module ApplicationHelper
     return content_tag("div", flash[:warning], :class => "warning") if flash[:warning]
   end
   
+  def generate_html(form_builder, method, options = {})
+    options[:object] ||= form_builder.object.class.reflect_on_association(method).klass.new if method
+    options[:partial] ||= method.to_s.singularize
+    options[:form_builder_local] ||= :f 
+    template = form_builder.fields_for(method, options[:object], :child_index => 'NEW_RECORD') do |f|
+      render(:partial => options[:partial], :locals => { options[:form_builder_local] => f, :show_delete => true })
+    end
+    template.html_safe
+  end
+
+  def generate_template(form_builder, method, options = {})
+    escape_javascript generate_html(form_builder, method, options)
+  end
+  
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,24 @@
+$(document).ready(function() {
+    $('.add').live('click', function() { Group.addItem(this); })
+    $('.delete').live('click', function() { Group.deleteItem(this); })
+})
 
+Group = {
+    addItem: function(item) {
+        template = eval(item.href.replace(/.*#/, ''));
+        template = replace_ids(template);
+        element = $(item).parent().before(template);
+        return false;
+    },
+
+    deleteItem: function(item) {
+        $(item).parent().remove();
+    }
+}
+
+
+replace_ids = function(s) {
+    var new_id = new Date().getTime();
+    return s.replace(/NEW_RECORD/g, new_id);
+}
 
