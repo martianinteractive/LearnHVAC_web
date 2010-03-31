5d27524a55cd036269147e4931e632ecc875e00a
jpemberthy <jpemberthy@gmail.com>
2010-03-31 16:55:46 -0500

    group accepts_nested_attributes_for group_scenario

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,14 +1,15 @@
 class Group < ActiveRecord::Base
   belongs_to :instructor, :class_name => "User", :foreign_key => "instructor_id"
   has_many :memberships, :dependent => :destroy
-  has_many :students, :through => :memberships
-  
+  has_many :students, :through => :memberships  
   has_many :group_scenarios
   
   validates :name, :presence => true, :length => { :maximum => 200 }, :uniqueness => true
   validates :code, :presence => true, :length => { :maximum => 200 }, :uniqueness => true, :on => :update
   validates :instructor, :presence => true
   
+  accepts_nested_attributes_for :group_scenarios, :allow_destroy => true
+
   after_create :set_code
     
   # Define this later using has_many_documents :scenarios, :through => :group_scenarios
