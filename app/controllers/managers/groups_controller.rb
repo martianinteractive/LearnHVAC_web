cd3f9c35394d2eef4ca33ca8abd5d07e4cddb9d3
jpemberthy <jpemberthy@gmail.com>
2010-04-06 17:49:24 -0500

    Disabling groups management for institutions_manager (for now)

--- app/controllers/managers/groups_controller.rb
+++ app/controllers/managers/groups_controller.rb
@@ -1,5 +1,5 @@
 class Managers::GroupsController < Managers::ApplicationController
-  before_filter :find_group, :only => [:show, :edit, :update, :destroy]
+  before_filter :find_group, :only => [:show]
   
   def index
     @groups = current_user.institution.groups.paginate :page => params[:page], :per_page => 25, :order => "users.last_name"
@@ -8,37 +8,37 @@ class Managers::GroupsController < Managers::ApplicationController
   def show
   end
   
-  def new
-    @instructor = current_user.institution.users.instructor.first
-    @group = Group.new(:instructor => @instructor)
-  end
-  
-  def edit
-  end
-  
-  def create
-    @group = Group.new(params[:group])
-
-    if @group.save
-      redirect_to(managers_group_path(@group), :notice => 'Group was successfully created.')
-    else
-      @instructor = current_user.institution.users.instructor.find(params[:group][:instructor_id])
-      render :action => "new"
-    end
-  end
-  
-  def update
-    if @group.update_attributes(params[:group])
-      redirect_to(managers_group_path(@group), :notice => 'Group was successfully updated.')
-    else
-      render :action => "edit"
-    end
-  end
-  
-  def destroy
-    @group.destroy
-    redirect_to(managers_groups_path)
-  end
+  # def new
+  #   @instructor = current_user.institution.users.instructor.first
+  #   @group = Group.new(:instructor => @instructor)
+  # end
+  # 
+  # def edit
+  # end
+  # 
+  # def create
+  #   @group = Group.new(params[:group])
+  # 
+  #   if @group.save
+  #     redirect_to(managers_group_path(@group), :notice => 'Group was successfully created.')
+  #   else
+  #     @instructor = current_user.institution.users.instructor.find(params[:group][:instructor_id])
+  #     render :action => "new"
+  #   end
+  # end
+  # 
+  # def update
+  #   if @group.update_attributes(params[:group])
+  #     redirect_to(managers_group_path(@group), :notice => 'Group was successfully updated.')
+  #   else
+  #     render :action => "edit"
+  #   end
+  # end
+  # 
+  # def destroy
+  #   @group.destroy
+  #   redirect_to(managers_groups_path)
+  # end
   
   
   private
