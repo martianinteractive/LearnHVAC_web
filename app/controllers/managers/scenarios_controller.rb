4faf3f382744bce17abfc9d099f2080ca3e06614
jpemberthy <jpemberthy@gmail.com>
2010-04-07 17:35:20 -0500

    managers/scenarios

--- app/controllers/managers/scenarios_controller.rb
+++ app/controllers/managers/scenarios_controller.rb
@@ -1,5 +1,13 @@
 class Managers::ScenariosController < Managers::ApplicationController
   
+  def index
+    @scenarios = current_user.institution.scenarios.paginate :page => params[:page], :per_page => 25
+  end
+  
+  def show
+    @scenario = current_user.institution.scenarios.find { |s| s.id == params[:id] }
+  end
+  
   def list
     @scenarios = current_user.institution.users.instructor.find(params[:user_id]).scenarios if params[:user_id]
     render :layout => false
