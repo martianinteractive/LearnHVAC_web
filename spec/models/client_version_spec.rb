248e1a646f86e482312c4eb6aeec943d8856b5a1
jpemberthy <jpemberthy@gmail.com>
2010-04-13 16:28:24 -0500

    basic spec for client_version.

--- spec/models/client_version_spec.rb
+++ spec/models/client_version_spec.rb
@@ -1,5 +1,20 @@
-require 'spec_helper'
+require File.dirname(__FILE__) + "/../spec_helper"
 
 describe ClientVersion do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @client_version = Factory(:client_version)
+  end
+  
+  it "" do
+    @client_version.should be_valid
+  end
+  
+  it "" do
+    client_version = Factory.build(:client_version)
+    client_version.should_not be_valid
+    client_version.errors[:version].should_not be_empty
+    client_version.version = "0.99.87"
+    client_version.should be_valid
+  end
+  
 end
