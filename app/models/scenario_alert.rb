6f536b1e6b3073b94b9202c739aed0f0bae10d23
jpemberthy <jpemberthy@gmail.com>
2010-06-16 15:45:48 -0500

    don't break alerts on production.

--- app/models/scenario_alert.rb
+++ app/models/scenario_alert.rb
@@ -13,6 +13,7 @@ class ScenarioAlert
   
   def master_scenario
     ms = MasterScenario.for_display(master_scenario_id.to_s, :add => :versions)
-    ms.version == master_scenario_version ? ms : ms.versions.detect { |v| v.version == master_scenario_version }
+    #condition is only necessary to support already created master_scenario.scenarios ...
+    ms.version == master_scenario_version ? ms : ms.versions.detect { |v| v.version == master_scenario_version } if ms
   end
 end
