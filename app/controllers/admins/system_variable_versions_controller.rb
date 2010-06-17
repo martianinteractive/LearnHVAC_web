068c9234a9467bc38989b2c4a3996b992f5afc46
jpemberthy <jpemberthy@gmail.com>
2010-06-17 16:27:18 -0500

    revisions/vars ui.

--- app/controllers/admins/system_variable_versions_controller.rb
+++ app/controllers/admins/system_variable_versions_controller.rb
@@ -1,7 +1,7 @@
 class Admins::SystemVariableVersionsController < Admins::ApplicationController 
   helper :sort
   include SortHelper
-  before_filter :find_master_scenario_and_version
+  before_filter :find_master_scenario_and_version, :add_crumbs
   before_filter :initialize_variables_sort, :only => [:index]
   
   def index
@@ -10,6 +10,7 @@ class Admins::SystemVariableVersionsController < Admins::ApplicationController
   
   def show
     @system_variable_version = @version.system_variables.find(params[:id])
+    add_crumb @system_variable_version.name, admins_master_scenario_revision_variable_path(@master_scenario, @version.version, @system_variable_version)
   end
   
   private
@@ -17,6 +18,13 @@ class Admins::SystemVariableVersionsController < Admins::ApplicationController
   def find_master_scenario_and_version
     @master_scenario = MasterScenario.for_display(params[:master_scenario_id], :add => :versions)
     @version = @master_scenario.versions.detect { |v| v.version.to_s == params[:revision_id] }
+  end
+  
+  def add_crumbs
+    add_crumb "Master Scenarios", admins_master_scenarios_path
+    add_crumb @master_scenario.name, admins_master_scenario_path(@master_scenario)
+    add_crumb "Revisions", admins_master_scenario_revisions_path(@master_scenario)
+    add_crumb "Variables", admins_master_scenario_revision_variables_path(@master_scenario, @version.version)
   end
   
 end
