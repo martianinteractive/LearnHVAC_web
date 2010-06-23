17d6a4d2002204d7f7b33772d65d09e95424b178
jpemberthy <jpemberthy@gmail.com>
2010-06-23 15:49:56 -0500

    fixing dashboard crumb path for managers.

--- app/controllers/managers/application_controller.rb
+++ app/controllers/managers/application_controller.rb
@@ -1,6 +1,5 @@
 class Managers::ApplicationController < ApplicationController
   before_filter :require_manager
   layout "managers"
-  
-    add_crumb "Dashboard", '/admins/dashboard'
+  add_crumb "Dashboard", "/managers/dashboard"
 end
