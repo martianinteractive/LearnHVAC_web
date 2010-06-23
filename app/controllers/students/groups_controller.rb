63e2c6904773f9478025340858f29153c2e21698
jpemberthy <jpemberthy@gmail.com>
2010-06-23 18:29:03 -0500

    studentes/groups ui.

--- app/controllers/students/groups_controller.rb
+++ app/controllers/students/groups_controller.rb
@@ -1,11 +1,13 @@
 class Students::GroupsController < Students::ApplicationController
-    
+  add_crumb("Groups") { |instance| instance.send :students_groups_path }
+  
   def index
     @groups = current_user.groups.paginate :page => params[:page], :per_page => 25
   end
   
   def show
     @group = current_user.groups.find(params[:id])
+    add_crumb @group.name, students_group_path(@group)
   end
   
 end
