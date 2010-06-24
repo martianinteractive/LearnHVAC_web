ed6814ce62fd764636cc2e5fa913b5c2801c63f8
jpemberthy <jpemberthy@gmail.com>
2010-06-24 15:36:14 -0500

    any option to filter.

--- app/helpers/system_variables_helper.rb
+++ app/helpers/system_variables_helper.rb
@@ -3,7 +3,7 @@ module SystemVariablesHelper
   def filter_select_for(field, collection)
     current = params[:filter][field] if params[:filter]
     content_tag(:form, :action => admins_master_scenario_system_variables_path, :method => :get) do 
-      select_tag "filter[#{field}]", options_for_select(collection, current), :class => "filter"
+      select_tag "filter[#{field}]", options_for_select(%w(Any) + collection, current), :class => "filter"
     end
   end
   
