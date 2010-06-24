1999fc941dad97da51f00bb85fe1d1f8743fd298
jpemberthy <jpemberthy@gmail.com>
2010-06-24 16:09:02 -0500

    do not use forms inside the table, filter is now working via js.

--- app/helpers/system_variables_helper.rb
+++ app/helpers/system_variables_helper.rb
@@ -2,9 +2,7 @@ module SystemVariablesHelper
   
   def filter_select_for(field, collection)
     current = params[:filter][field] if params[:filter]
-    content_tag(:form, :action => admins_master_scenario_system_variables_path, :method => :get) do 
-      select_tag "filter[#{field}]", options_for_select(%w(Any) + collection, current), :class => "filter"
-    end
+    select_tag "filter[#{field}]", options_for_select(%w(Any) + collection, current), :class => "filter"
   end
   
 end
