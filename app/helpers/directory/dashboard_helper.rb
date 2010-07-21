304784e1016ac333dbf35af2a98d31c7b8c30a1d
jpemberthy <jpemberthy@gmail.com>
2010-07-21 17:59:21 -0500

    Fixing role_and_institution_for(user)

--- app/helpers/directory/dashboard_helper.rb
+++ app/helpers/directory/dashboard_helper.rb
@@ -1,5 +1,9 @@
 module Directory::DashboardHelper
   def role_and_institution_for(user)
-    user.has_role?(:admin) ? "admin" : "#{user.role} at #{formatted_institution_name(user.institution)}"
+    if user.has_role?(:admin) or user.has_role?(:guest) 
+      user.role
+    else
+      "#{user.role} at #{formatted_institution_name(user.institution)}"
+    end
   end
 end
