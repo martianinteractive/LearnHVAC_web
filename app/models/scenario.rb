2c11ddb4cc807f93afba26a677dbdcee6526de1e
jpemberthy <jpemberthy@gmail.com>
2010-07-28 17:22:05 -0500

    Scenario callback to copy sys vars.

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -10,49 +10,35 @@ class Scenario < ActiveRecord::Base
   
   validates_presence_of :name, :master_scenario_id, :user, :longterm_start_date, :longterm_stop_date, :realtime_start_date
   validates_format_of :longterm_start_date, :longterm_stop_date, :realtime_start_date, :with => /\d{2}\/\d{2}\/\d{4}/, :message => "is invalid"
-  # validate :longterm_validator
+  validate :longterm_validator
   
   scope :recently_created, where(["created_at > ?", 30.days.ago.utc])
   scope :recently_updated, where(["updated_at > ?", 30.days.ago.utc])
   scope :public, where(:public => true)
   scope :with_unread_alerts, where("scenario_alerts.read" => false)
   
-  # belongs_to_related :user
-  # belongs_to_related :master_scenario
-  # belongs_to_related :client_version
-  # has_many_related :group_scenarios
-  # 
-  # before_create :copy_master_scenario_attributes
-  # after_create :copy_system_variables
-  # 
+  after_create :copy_variables
+  
+  private
+  
+  def copy_variables
+    master_scenario.variables.each do |sys_var|
+      variables.create sys_var.attributes.except("id", "created_at", "updated_at", "master_scenario_id")
+    end
+  end
+  
+  def longterm_validator
+    errors.add(:longterm_start_date, "should be set before the longterm stop date") if longterm_start_date >= longterm_stop_date
+    errors.add(:longterm_stop_date, "should be set after the longterm start date") if longterm_stop_date <= longterm_start_date
+    errors.add(:realtime_start_date, "should be set between start and stop dates") if (realtime_start_date < longterm_start_date) or (realtime_start_date > longterm_stop_date) 
+  end
+  
   # def groups
   #   groups_ids = group_scenarios.collect { |gs| gs.group_id }
   #   Group.find(groups_ids)
   # end
-  # 
-  # def master_scenario
-  #   begin
-  #     ms = MasterScenario.find(self.master_scenario_id)
-  #     ms_version = self.master_scenario_version
-  #     (self.new_record? or ms.version == ms_version) ? ms : ms.versions.detect { |v| v.version == ms_version }
-  #   rescue
-  #     nil
-  #   end
-  # end
   #   
   # private
-  #   
-  # def longterm_validator
-  #   errors.add(:longterm_start_date, "should be set before the longterm stop date") if longterm_start_date >= longterm_stop_date
-  #   errors.add(:longterm_stop_date, "should be set after the longterm start date") if longterm_stop_date <= longterm_start_date
-  #   errors.add(:realtime_start_date, "should be set between start and stop dates") if (realtime_start_date < longterm_start_date) or (realtime_start_date > longterm_stop_date) 
-  # end
-  # 
-  # def copy_master_scenario_attributes
-  #   self.master_scenario_version = master_scenario.version
-  #   self.master_scenario_name = master_scenario.name
-  #   self.client_version_id = master_scenario.client_version.id
-  # end
   # 
   # def copy_system_variables
   #   self.master_scenario.system_variables.each do |sv|
