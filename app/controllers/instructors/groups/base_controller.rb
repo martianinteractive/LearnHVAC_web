1761ae1c58710138c79486943bca6b7d0c90a6b8
jpemberthy <jpemberthy@gmail.com>
2010-07-28 09:55:07 -0500

    instructors/groups/emails actions.

--- app/controllers/instructors/groups/base_controller.rb
+++ app/controllers/instructors/groups/base_controller.rb
@@ -1,2 +1,10 @@
 class Instructors::Groups::BaseController < Instructors::ApplicationController
+  before_filter :find_group
+  
+  private
+  
+  def find_group
+    @group = current_user.managed_groups.find(params[:group_id])
+  end
+  
 end
