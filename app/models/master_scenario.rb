7b7f6822af509806205c7551462a44976e078c57
jpemberthy <jpemberthy@gmail.com>
2010-07-29 12:02:57 -0500

    master_scenario.clone!

--- app/models/master_scenario.rb
+++ app/models/master_scenario.rb
@@ -9,33 +9,17 @@ class MasterScenario < ActiveRecord::Base
   validates_presence_of :name, :user, :client_version
   validates_associated :client_version
   
+  def clone!
+    clon_atts = attributes.except("id", "created_at", "updated_at")
+    clon = MasterScenario.create(clon_atts.merge("name" => "#{name}_clone"))
+    variables.each { |var| clon.variables.create var.attributes.except("id", "scenario_id", "created_at", "updated_at") }
+    clon
+  end
   
   # before_save :delete_version_note
   # after_create :create_initial_version_note  
   # embed_many :system_variables
   # embed_one :version_note
-  
-  # has_many_related :scenarios
-  # belongs_to_related :user
-  # belongs_to_related :client_version
-  # 
-  # index :user_id
-  # index :client_version_id
-  
-  # def clone!(usr=nil)
-  #   clon_atts = self.attributes.except("_id", "user_id").merge(default_clon_attributes)
-  #   clon = MasterScenario.new(clon_atts)
-  #   clon.user = usr || self.user
-  #   sys_vars = []
-  #   self.variables.each { |sv| sys_vars << sv.attributes.except("_id") }
-  #   clon.system_variables = sys_vars
-  #   clon.save
-  #   clon
-  # end
-  #   
-  # def default_clon_attributes
-  #   { "version" => 1, "versions" => nil, "system_variables" => nil, "name" => "#{self.name}_clon" }
-  # end
   # 
   # private
   # 
