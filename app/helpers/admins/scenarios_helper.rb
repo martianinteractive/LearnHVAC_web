89d79480ea60b3a473447d60b6de8ba222764500
jpemberthy <jpemberthy@gmail.com>
2010-07-29 18:54:35 -0500

    do not support master scenario versions (for now).

--- app/helpers/admins/scenarios_helper.rb
+++ app/helpers/admins/scenarios_helper.rb
@@ -1,5 +1,5 @@
 module Admins::ScenariosHelper
   def link_to_master_scenario_for(scenario)
-    "master scenario link"
+    scenario.master_scenario ? link_to(scenario.master_scenario.name, [:admins, scenario.master_scenario]) : "deleted"
   end
 end
