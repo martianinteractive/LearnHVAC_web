551a7c79bbb0f66db2d2f45db4662ecb2ac4fc0b
jpemberthy <jpemberthy@gmail.com>
2010-07-29 12:02:27 -0500

    variables, scenarios presence validations.

--- app/models/scenario_variable.rb
+++ app/models/scenario_variable.rb
@@ -1,3 +1,5 @@
 class ScenarioVariable < Variable
   belongs_to :scenario
+  
+  validates_presence_of :scenario
 end
--- app/models/system_variable.rb
+++ app/models/system_variable.rb
@@ -1,3 +1,5 @@
 class SystemVariable < Variable
-  belongs_to :master_scenario, :foreign_key => "scenario_id"   
+  belongs_to :master_scenario, :foreign_key => "scenario_id" 
+  
+  validates_presence_of :master_scenario  
 end
