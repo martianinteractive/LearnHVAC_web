adf6c8d18661f7b31251b059d774a5794c6069dc
jpemberthy <jpemberthy@gmail.com>
2010-07-30 15:47:19 -0500

    do not use deprecated request.request_uri

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -73,7 +73,7 @@ class ApplicationController < ActionController::Base
       flash[:notice] = "You don't have privileges to access that page"
       redirect_back_or_default(default_path_for(user))
     else
-      store_location if request.request_uri != "/"
+      store_location if request.fullpath != "/"
       flash[:notice] = "You must be logged in to access this page"
       redirect_to login_path
       return false
