7c35e5405dd2ef464484a24e3d0acc3e1de0afb9
Sergio Bayona <sbayona@martianinteractive.com>
2010-08-01 13:49:37 -0400

    use these factories

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -10,3 +10,73 @@ Factory.define :user do |user|
   user.city 'Bethesda'
   user.state 'Maryland'
 end
+
+Factory.define :student, :class => User do |user|
+  user.login 'peter'
+  user.first_name 'Peter'
+  user.last_name 'Doe'
+  user.email 'peter@builder.com'
+  user.password 'peter222'
+  user.password_confirmation 'peter222'
+  user.role_code 1
+  user.enabled true
+  user.country 'United States'
+  user.city 'Bethesda'
+  user.state 'Maryland'
+end
+
+Factory.define :instructor, :class => User do |user|
+  user.login 'joeyb'
+  user.first_name 'Joe'
+  user.last_name 'Boe'
+  user.email 'jboe@builder.com'
+  user.password 'jboe333'
+  user.password_confirmation 'jboe333'
+  user.role_code 2
+  user.enabled true
+  user.country 'United States'
+  user.city 'Bethesda'
+  user.state 'Maryland'
+end
+
+Factory.define :manager, :class => User do |user|
+  user.login 'loreng'
+  user.first_name 'loren'
+  user.last_name 'Goey'
+  user.email 'joreng@builder.com'
+  user.password 'jboe333'
+  user.password_confirmation 'jboe333'
+  user.role_code 3
+  user.enabled true
+  user.country 'United States'
+  user.city 'Bethesda'
+  user.state 'Maryland'
+end
+
+Factory.define :admin, :class => User do |user|
+  user.login 'tonyal'
+  user.first_name 'tonya'
+  user.last_name 'love'
+  user.email 'tolove@builder.com'
+  user.password 'blawasdf'
+  user.password_confirmation 'blawasdf'
+  user.role_code 4
+  user.enabled true
+  user.country 'United States'
+  user.city 'Bethesda'
+  user.state 'Maryland'
+end
+
+Factory.define :guest, :class => User do |user|
+  user.login 'louist'
+  user.first_name 'louis'
+  user.last_name 'Troy'
+  user.email 'louist@builder.com'
+  user.password 'blawasdf'
+  user.password_confirmation 'blawasdf'
+  user.role_code 0
+  user.enabled true
+  user.country 'United States'
+  user.city 'Bethesda'
+  user.state 'Maryland'
+end
