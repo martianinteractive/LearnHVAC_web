5be636a2438d5d0da5dc0c59b466fb8b0d06f2aa
jpemberthy <jpemberthy@gmail.com>
2010-08-02 15:12:06 -0500

    removing memberships from managers/

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -5,6 +5,8 @@ class Membership < ActiveRecord::Base
   validates_presence_of :group, :member
   validates_uniqueness_of :member_id, :scope => :group_id 
   
+  scope :instructor_students, joins(:member).where("users.role_code in (?)", [User::ROLES[:student], User::ROLES[:admin]])
+  
   def recently_created?
     created_at > 20.minutes.ago
   end
