8b6dedb52f1b62919b9461c9b79a4b119d43dfef
jpemberthy <jpemberthy@gmail.com>
2010-08-02 12:46:09 -0500

    managers/groups spec render views.

--- spec/controllers/managers/groups_controller_spec.rb
+++ spec/controllers/managers/groups_controller_spec.rb
@@ -1,6 +1,8 @@
 require File.dirname(__FILE__) + "/../../spec_helper"
 
 describe Managers::GroupsController do
+  render_views
+  
   before(:each) do
     institution   = Factory(:institution)
     @manager      = Factory(:manager, :institution => institution)
