5d96c89101eb1b525b46ef713ba2c1a2c6124edc
jpemberthy <jpemberthy@gmail.com>
2010-08-04 15:10:22 -0500

    refactoring.

--- app/controllers/admins/memberships_controller.rb
+++ app/controllers/admins/memberships_controller.rb
@@ -1,9 +1,8 @@
 class Admins::MembershipsController < Admins::ApplicationController
   
   def destroy
-    @user = User.find(params[:id])
     @group = Group.find(params[:class_id])
-    memberships = @group.memberships.where(:member_id => @user.id)
+    memberships = @group.memberships.where(:member_id => params[:id])
     memberships.each { |m| m.destroy }
     redirect_to admins_class_path(@group)
   end
