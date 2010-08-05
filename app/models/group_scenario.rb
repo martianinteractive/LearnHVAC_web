4c3de9154b2d8f8e802c8000d57b7fbc553d815b
jpemberthy <jpemberthy@gmail.com>
2010-08-05 15:55:52 -0500

    after create group_scenario create memberships for the group students.

--- app/models/group_scenario.rb
+++ app/models/group_scenario.rb
@@ -3,7 +3,14 @@ class GroupScenario < ActiveRecord::Base
   belongs_to :scenario
   
   validates_presence_of :group, :scenario
-    
-  # This validation is being ignored from groups#create/update
   validates_uniqueness_of :scenario_id, :scope => :group_id
+  
+  after_create :create_memberships
+  
+  private
+  
+  def create_memberships
+    group.members.each { |member| group.memberships.create(:scenario => self.scenario, :member => member) }
+  end
+  
 end
