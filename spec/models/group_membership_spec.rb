3efee9038aedd810022a43315a5d96d2f4c01aa6
jpemberthy <jpemberthy@gmail.com>
2010-08-05 11:57:05 -0500

    cleaning.

--- spec/models/group_membership_spec.rb
+++ spec/models/group_membership_spec.rb
@@ -19,28 +19,5 @@ describe GroupMembership do
     group_membership.should_not be_valid
     group_membership.errors.values.to_s.should == "has already been taken"
   end
-  
-  describe ".split!" do
-    before(:each) do
-      @scenario_2 = Factory(:scenario, :master_scenario => @ms, :user => @instructor, :name => 'scenario_2')
-      @scenario_3 = Factory(:scenario, :master_scenario => @ms, :user => @instructor, :name => 'scenario_3')
-      
-      Factory(:group_scenario, :group => @group, :scenario => @scenario)
-      Factory(:group_scenario, :group => @group, :scenario => @scenario_2)
-      Factory(:group_scenario, :group => @group, :scenario => @scenario_3)
-      @group.reload
-    end
     
-    it "should create new group_memberships based on the associated group scenarios" do
-      proc { @group_membership.split! }.should change(GroupMembership, :count).by(3)
-    end
-    
-    it "should assign each scenario to group_memberships" do
-      group_memberships = @group_membership.split!
-      group_memberships.first.scenario.should == @scenario
-      group_memberships.second.scenario.should == @scenario_2
-      group_memberships.third.scenario.should == @scenario_3
-    end
-  end
-  
 end
