ecd17ec397f92e9e6d7edadb5307a5f9d4bdc012
jpemberthy <jpemberthy@gmail.com>
2010-08-06 10:35:59 -0500

    cleaning.

--- app/models/group_membership.rb
+++ app/models/group_membership.rb
@@ -1,13 +1,4 @@
 class GroupMembership < Membership  
   validates_presence_of :group
-  validates_uniqueness_of :member_id, :scope => [:group_id, :scenario_id]
-      
-  def split!
-    return false unless group and member 
-    group.scenarios.each do |s|
-      self.class.create(:member => self.member, :group => self.group, :scenario => s)
-    end
-    group.memberships.where(:member_id => member.id)
-  end
-  
+  validates_uniqueness_of :member_id, :scope => [:group_id, :scenario_id]  
 end
