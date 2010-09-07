0ecb72b7f46e8cec434015da0a78ff935f215de0
jpemberthy <jpemberthy@gmail.com>
2010-09-07 14:49:04 -0500

    user sweeper hooks for scenarios.

--- app/sweeperes/user_sweeper.rb
+++ app/sweeperes/user_sweeper.rb
@@ -3,13 +3,26 @@ class UserSweeper < ActionController::Caching::Sweeper
   observe User
   
   def after_save(user)
+    expire_user_actions(user)
+    expire_scenarios(user.all_scenarios)
+  end
+  
+  def after_destroy(user)
+    expire_user_actions(user)
+    expire_scenarios(user.all_scenarios)
+  end
+  
+  private
+  
+  def expire_user_actions(user)
     expire_action("profile/#{user.login}")
     expire_action(admins_users_path(:role => user.role))
   end
   
-  def after_destroy(user)
-    expire_action("profile/#{user.login}")
-    expire_action(admins_users_path(:role => user.role))
+  def expire_scenarios(scenarios)
+    scenarios.each do |scenario|
+      expire_action(admins_scenario_accesses_path(scenario))
+    end
   end
   
 end
