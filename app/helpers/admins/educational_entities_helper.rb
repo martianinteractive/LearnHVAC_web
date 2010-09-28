1f855d52aecf9ea47476e3198c9cc63ba0d5a00e
jpemberthy <jpemberthy@gmail.com>
2010-09-28 12:42:35 -0500

    fixing pagination for search results in admin/educational_entities.

--- app/helpers/admins/educational_entities_helper.rb
+++ app/helpers/admins/educational_entities_helper.rb
@@ -1,2 +1,11 @@
 module Admins::EducationalEntitiesHelper
+  
+  def paginate
+    if params[:action] == "index"
+      will_paginate @colleges
+    else
+      will_paginate @colleges, :renderer => "PostLinkRenderer", :params => { :q => params[:q] }
+    end
+  end
+  
 end
