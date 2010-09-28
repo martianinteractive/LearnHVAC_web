4192e5a8465a42e52f13302d0eb14add3dabeaab
jpemberthy <jpemberthy@gmail.com>
2010-09-28 18:50:34 -0500

    users index on role_code.

--- db/schema.rb
+++ db/schema.rb
@@ -1,8 +1,8 @@
-# This file is auto-generated from the current state of the database. Instead 
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your 
+# Note that this schema.rb definition is the authoritative source for your
 # database schema. If you need to create the application database on another
 # system, you should be using db:schema:load, not running all the migrations
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100813145518) do
+ActiveRecord::Schema.define(:version => 20100928160458) do
 
   create_table "class_notification_emails", :force => true do |t|
     t.integer  "class_id"
@@ -184,6 +184,7 @@ ActiveRecord::Schema.define(:version => 20100813145518) do
   end
 
   add_index "users", ["perishable_token"], :name => "index_users_on_perishable_token"
+  add_index "users", ["role_code"], :name => "index_users_on_role_code"
 
   create_table "variables", :force => true do |t|
     t.string   "name"
