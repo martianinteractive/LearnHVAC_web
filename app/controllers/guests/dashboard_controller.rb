3d1a2424fb864d81a2101c4f3ec11566ac31ad4b
jpemberthy <jpemberthy@gmail.com>
2010-10-04 18:06:54 -0500

    As a guest user who just submitted the registration form, I should be able to immediately download the client app.

--- app/controllers/guests/dashboard_controller.rb
+++ app/controllers/guests/dashboard_controller.rb
@@ -1,9 +1,15 @@
 class Guests::DashboardController < ApplicationController
-  before_filter :require_guest
+  before_filter :require_guest_or_token
   layout "guests"
   
   def show
     @client_versions = ClientVersion.paginate :page => params[:page], :per_page => 10, :order => "release_date DESC"
+  end
+  
+  private
+  
+  def require_guest_or_token
+    require_guest if logged_in? or !User.guest.find_by_perishable_token(params[:token])
   end
   
 end
--- spec/controllers/guests/dashboard_controller_spec.rb
+++ spec/controllers/guests/dashboard_controller_spec.rb
@@ -4,14 +4,26 @@ describe Guests::DashboardController do
   before(:each) do
     @client_version = Factory(:client_version)
     @guest          = Factory(:guest)
-    login_as(@guest)
   end
   
   describe "GET :show" do
-    it "should show the client versions" do
+    it "" do
+      login_as(@guest)
       get :show
       response.should render_template(:show)
       assigns(:client_versions).should_not be_empty
+    end
+    
+    it "should allow authorization via perishable token" do
+      get :show, :token => @guest.perishable_token
+      response.should render_template(:show)
+      assigns(:client_versions).should_not be_empty
+    end
+    
+    it "should not allow authorization for an invalid perishable token" do
+      student = Factory(:student)
+      get :show, :token => student.perishable_token
+      response.should redirect_to(login_path)
     end
   end
 end
