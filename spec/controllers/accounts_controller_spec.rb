dea828825e34d0b7e3b6b7c9d960f3816f310e69
Sergio Bayona <sbayona@martianinteractive.com>
2011-01-26 11:48:44 -0500

    rspec refactoring

--- spec/controllers/accounts_controller_spec.rb
+++ spec/controllers/accounts_controller_spec.rb
@@ -41,65 +41,55 @@ describe AccountsController do
         proc { post :create, :user => Factory.attributes_for(:user) }.should change(ActionMailer::Base.deliveries, :size).by(1)
       end
     end
+    
+    context "an invalid account" do
+      let(:user) {mock_model(User, :save_without_session_maintenance => false, :deliver_activation_instructions! => true, :has_role? => true, :active= => false, :role_code= => nil, :require_agreement_acceptance! => true)}
+      
+      it "should expose a newly created but unsaved user as @account" do
+        User.stub!(:new).with({'role_code' => '2'}).and_return(user)
+        post :create, :user => {:role_code => '2'}
+        assigns(:account).should equal(user)
+      end
+      
+      it "should re-render the new template" do
+        User.stub!(:new).with({'role_code' => '2'}).and_return(user)
+        post :create, :user => {:role_code => '2'}
+        response.should render_template('new')
+      end     
+    end
   end
-  # 
-  # describe "POST :create" do
-  #   before(:each) do
-  #     ActionMailer::Base.deliveries = []
-  #   end
-  #   
-  #   describe "a valid account" do
-  #     it "should save an non-active account" do
-  #       post :create, :user => Factory.attributes_for(:user)
-  #       assigns(:account).active?.should_not be(true)
-  #     end
-  #     
-  #     it "should save set the role as :instructor" do
-  #       post :create, :user => Factory.attributes_for(:user)
-  #       assigns(:account).role_code.should == User::ROLES[:instructor]
-  #     end
-  #     
-  #     it "should send an activation information mail" do
-  #       proc { post :create, :user => Factory.attributes_for(:user) }.should change(ActionMailer::Base.deliveries, :size).by(1)
-  #     end
-  #     
-  #     it "should redirect instructors to the login action" do
-  #       post :create, :user => Factory.attributes_for(:user)
-  #       response.should redirect_to(login_path)
-  #     end
-  #     
-  #     it "should redirect guests to guests/dashboard" do
-  #       post :create, :user => Factory.attributes_for(:user).merge({:role_code => User::ROLES[:guest]})
-  #       response.should redirect_to(guests_dashboard_path(:token => assigns(:account).perishable_token))
-  #     end
-  #     
-  #   end
-  #   
-  #   describe "an invalid account" do
-  #     it "" do
-  #       post :create, :user => {}
-  #       response.should render_template(:new)
-  #     end
-  #     
-  #     it "due to an invalid role_code" do
-  #       post :create, :user => Factory.attributes_for(:user).merge({:role_code => User::ROLES[:admin]})
-  #       response.should render_template(:new)
-  #     end
-  #   end
-  # end
-  # 
-  # describe "Authentication" do
-  #   before(:each) do
-  #     @user = Factory(:admin)
-  #     login_as @user
-  #   end
-  #   
-  #   it "should require NO user" do
-  #     authorize_actions({}, {:get => [:new], :post => [:create]}) do
-  #       response.should be_redirect
-  #       flash[:notice].should == "You must be logged out to access this page"
-  #     end
-  #   end
-  # end
+  
+  context "Get: colleges" do
+    let(:colleges) { [mock_model(College, :value => "UCLA")] }
+    
+    it "should expose colleges as @colleges" do
+      College.should_receive(:find).and_return(colleges)
+      get :colleges, :term => 'UCLA'
+      assigns[:colleges].should eq(colleges)
+    end
+    
+    it "should render json" do
+      College.stub(:find).and_return(colleges)
+      get :colleges, :term => 'UCLA'
+      response.body.should == "[\"UCLA\"]"
+    end
+  end
+  
+  context "Get: states" do
+    let(:states) { [mock_model(Region)] }
+    
+    it "should expose states as @states" do
+      Region.stub_chain(:where, :order => states)
+      Region.should_receive(:where).with({:country => "US"})
+      get :states, :state => 'United States'
+      assigns[:states].should eq(states)
+    end
+    
+    it "should render partial states" do
+      Region.stub_chain(:where, :order => states)
+      get :states, :state => "United States"
+      response.should render_template('states')
+    end
+  end
   
 end
