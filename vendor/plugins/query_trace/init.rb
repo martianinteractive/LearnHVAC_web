0ed64614b3a4d44e93c697b83a20015ef8ef3dda
Sergio Bayona <sbayona@martianinteractive.com>
2011-01-26 16:05:02 -0500

    updated query_trace

--- vendor/plugins/query_trace/init.rb
+++ vendor/plugins/query_trace/init.rb
@@ -1,16 +1,5 @@
 require 'query_trace'
 
-def status
-  QueryTrace.enabled? ? "enabled" : "disabled"
-end
-
-puts "=> QueryTrace #{status}; CTRL-\\ to toggle"
-
-trap("QUIT") do
-  # Sending 2 backspace characters removes the ^\ that is 
-  # printed to the console.
-  rm_noise = "\b\b"
-
-  QueryTrace.toggle!
-  puts "#{rm_noise}=> QueryTrace #{status}"
+class ::ActiveRecord::ConnectionAdapters::AbstractAdapter
+  include QueryTrace
 end
--- vendor/plugins/query_trace/lib/query_trace.rb
+++ vendor/plugins/query_trace/lib/query_trace.rb
@@ -1,58 +1,45 @@
 module QueryTrace
-  mattr_accessor :depth
-  self.depth = 20
-  
-  def self.enabled?
-    defined?(@@trace_queries) && @@trace_queries
-  end
-     
-  def self.enable!
-    ::ActiveRecord::ConnectionAdapters::AbstractAdapter.send(:include, QueryTrace) unless defined?(@@trace_queries)
-    @@trace_queries = true
-  end
-  
-  def self.disable!
-    @@trace_queries = false
-  end
-
-  # Toggles query tracing on and off and returns a boolean indicating the new
-  # state of query tracing (true for enabled, false for disabled).
-  def self.toggle!
-    enabled? ? disable! : enable!
-    enabled?
-  end
-  
   def self.append_features(klass)
     super
     klass.class_eval do
-      unless method_defined?(:log_without_trace)
-        alias_method :log_without_trace, :log
-        alias_method :log, :log_with_trace
+      unless method_defined?(:log_info_without_trace)
+        alias_method :log_info_without_trace, :log_info
+        alias_method :log_info, :log_info_with_trace
       end
     end
+    klass.class_eval %(
+      def row_even
+        @@row_even
+      end
+    )
   end
+  
+  def log_info_with_trace(sql, name, runtime)
+    log_info_without_trace(sql, name, runtime)
+    
+    return unless @logger and @logger.debug?
+    return if / Columns$/ =~ name
 
-  def log_with_trace(sql, name, &block)
-    result = log_without_trace(sql, name, &block)
-
-    return result unless @@trace_queries
-
-    return result unless ActiveRecord::Base.logger and ActiveRecord::Base.logger.debug?
-    return result if / Columns$/ =~ name
-
-    ActiveRecord::Base.logger.debug(format_trace(Rails.backtrace_cleaner.clean(caller)[0..self.depth]))
-
-    result 
+    trace = clean_trace(caller[2..-1])
+    @logger.debug(format_trace(trace))
   end
-
+  
   def format_trace(trace)
-    if (defined?(ActiveRecord::LogSubscriber) ? ActiveRecord::LogSubscriber : ActiveRecord::Base).colorize_logging
-      message_color = "35;2"
+    if ActiveRecord::Base.colorize_logging
+      if row_even
+        message_color = "35;2"
+      else
+        message_color = "36;2"
+      end
       trace.collect{|t| "    \e[#{message_color}m#{t}\e[0m"}.join("\n")
     else
       trace.join("\n    ")
     end
   end
+  
+  def clean_trace(trace)
+    Rails.respond_to?(:backtrace_cleaner) ?
+      Rails.backtrace_cleaner.clean(trace) :
+      trace
+  end
 end
-
-QueryTrace.enable! if ENV["QUERY_TRACE"]
