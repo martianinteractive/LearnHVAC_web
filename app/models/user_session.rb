3af99e3a2879a1d9486e6bb9e68b219a28056987
Joan Ruiz <morden.curien@gmail.com>
2011-04-26 22:37:20 -0500

    Warning in login page for inactive users

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,11 +1,16 @@
 class UserSession < Authlogic::Session::Base
   before_validation :check_enabled
-  
+  validate :check_active
   private
   
   def check_enabled
     user = User.find_by_login(login) if login
     errors.add(:base, "Your account has been temporary disabled") if user and !user.enabled?
+  end
+
+  def check_active
+    user = User.find_by_login(login) if login
+    errors.add(:base, "Your account is not active. You should have received an activation email when you first signed up. Please follow the activation email instructions.") if user and !user.active
   end
   
   
