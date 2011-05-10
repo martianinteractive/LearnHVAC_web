7f1748eb90ceef804659660effd34537f06d7661
Joan Ruiz <morden.curien@gmail.com>
2011-05-10 14:31:16 -0500

    View all in community directory fixed

--- app/controllers/directory/people_controller.rb
+++ app/controllers/directory/people_controller.rb
@@ -2,7 +2,7 @@ class Directory::PeopleController < Directory::ApplicationController
   add_crumb("People") { |instance| instance.send :directory_people_path }
   
   def index
-    @users = User.paginate :page => params[:page], :per_page => 25, :order => "created_at DESC"
+    @users = User.listed_directory.paginate :page => params[:page], :per_page => 25, :order => "created_at DESC"
   end
   
   def show
