1e8584a604e0313966677257dfe89843e1479600
Joan Ruiz <morden.curien@gmail.com>
2011-05-23 13:53:38 -0500

    Alert pops when a instructor tries to change to guest user and has groups or scenarios

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,4 +6,14 @@ $(document).ready(function() {
 		$("#tabs").no_ajax_tabs();
 		$("#inner-tabs").no_ajax_tabs();
 	});
+        $('#user_role_code_0').click(function(){
+                groups = $('#groups');
+                scenarios = $('#scenarios');
+                if((groups.val()+scenarios.val()) > 0){
+                    var x = confirm('You have groups and/or scenarios. If you change your profile to guest, you will loose access to the groups. Are you sure?');
+                    if(!x){
+                        $('#user_role_code_2').click();
+                    }
+                }
+        });
 });
