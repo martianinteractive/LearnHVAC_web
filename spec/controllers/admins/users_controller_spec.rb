6bc02352c31b5e81ee2c0d097c586683c0c38da3
Carlos León <mail@carlosleon.info>
2011-10-19 16:57:04 -0500

    Fixing broken controller specs.

--- spec/controllers/admins/users_controller_spec.rb
+++ spec/controllers/admins/users_controller_spec.rb
@@ -2,177 +2,185 @@ require File.dirname(__FILE__) + "/../../spec_helper"
 
 describe Admins::UsersController do
   let(:current_user) { Factory.stub(:admin) }
-  
+
   def mock_user(stubs={})
     @mock_user ||= mock_model(User, {:name => ""}.merge(stubs))
   end
-  
+
   before { controller.stub(:current_user).and_return(current_user) }
-  
+
   context "GET index" do
     before do
       User.stub_chain(:where, :includes, :order, :paginate).and_return([mock_user])
     end
-    
+
     it "should expose users" do
       get :index, :role => 'admin', :requirements=>{:role => /[a-z]/}
       assigns[:users].should eq([mock_user])
     end
-    
+
     it "should render template" do
       get :index, :role => 'admin', :requirements=>{:role => /[a-z]/}
       response.should render_template(:index)
     end
   end
-  
+
   context "POST search" do
     before do
       User.stub_chain(:search, :paginate).and_return([mock_user])
     end
-    
+
     it "should expose users" do
       post :search, :q => "love", :role => 'admin', :requirements=>{:role => /[a-z]/} 
       assigns[:users].should eq([mock_user])
     end
-    
+
     it "should render index template" do
       post :search, :q => "love", :role => 'admin', :requirements=>{:role => /[a-z]/} 
       response.should render_template(:index)
     end
   end
-  
+
   context "POST filter" do
     before do
       User.stub_chain(:filter, :paginate).and_return([mock_user])
     end
-    
+
     it "should expose user" do
       post :filter, :institution_id => "1", :role => 'instructor', :requirements=>{:role => /[a-z]/}
       assigns[:users].should eq([mock_user])
     end
-    
+
     it "should render the index template" do
       post :filter, :institution_id => "1", :role => 'instructor', :requirements=>{:role => /[a-z]/}
       response.should render_template(:index)
     end
   end
-  
+
   describe "GET show" do
     before { User.stub(:find).with('1').and_return(mock_user) }
-    
+
     it "assigns the requested user as @user" do
       get :show, :id => "1", :role => 'admin', :requirements=>{:role => /[a-z]/}
       assigns[:user].should eq(mock_user)
     end
-    
+
     it "should render the show tempalte" do
       get :show, :id => "1", :role => 'student', :requirements => { :role => /[a-z]/ }
       response.should render_template(:show)
     end
   end
-  
+
   describe "GET edit" do
     before { User.stub(:find).with("1").and_return(mock_user) }
-    
+
     it "assigns the requested user as @user" do
       get :edit, :id => "1", :role => 'admin', :requirements=>{:role => /[a-z]/}
       assigns[:user].should eq(mock_user)
     end
-    
+
     it "should render the edit template" do
       get :edit, :id => "1", :role => 'admin', :requirements=>{:role => /[a-z]/}
       response.should render_template(:edit)
     end
   end
-  
+
   describe "POST create" do
     describe "successfully" do
       before do
-        mock_user(:role_code= => "", :enabled= => "", :save => true)
+        mock_user(
+          :role_code=   => "",
+          :enabled=     => "",
+          :save         => true,
+          :has_role?    => true,
+          :group_code=  => "",
+          :group_code   => ""
+        )
         User.stub(:new).with({'this' => 'params'}).and_return(mock_user)
+        Group.stub_chain(:find_by_code, :create_memberships).and_return(mock_model(Membership))
       end
-    
+
       it "should expose user" do
         post :create, :user => {:this => 'params'}, :role => "student", :requirements=>{:role => /[a-z]/}
         assigns[:user].should eq(mock_user)
       end
-      
+
       it "should redirect" do
         post :create, :user => {:this => 'params'}, :role => "student", :requirements=>{:role => /[a-z]/}
         response.should redirect_to(admins_user_path(assigns[:user], :role => "student", :anchor => "ui-tabs-1"))
       end
     end
-    
+
     describe "unsuccessfully" do
       before do
-        mock_user(:role_code= => "", :enabled= => "", :save => false)
+        mock_user(:role_code= => "", :enabled= => "", :save => false, :group_code= => '')
         User.stub(:new).with({'this' => 'params'}).and_return(mock_user)
       end
-      
+
       it "should expose the user" do
         post :create, :user => {:this => 'params'}, :role => "student", :requirements=>{:role => /[a-z]/}
         assigns[:user].should eq(mock_user)
       end
-      
+
       it "should render the new teplate" do
         post :create, :user => {:this => 'params'}, :role => "student", :requirements=>{:role => /[a-z]/}
         response.should render_template(:new)
       end
     end
   end
-  
+
   describe "PUT update" do
     describe "successfully" do
       before do
         mock_user(:role_code= => "", :enabled= => "", :update_attributes => true)
         User.stub(:find).with('1').and_return(mock_user)
       end
-      
+
       it "should expose user" do
         put :update, :id => "1", :user => { :first_name => "Joe" }, :role => "guest", :requirements=>{:role => /[a-z]/}
         assigns[:user].should eq(mock_user)
       end
-      
+
       it "should redirect" do
         put :update, :id => "1", :user => { :first_name => "Joe" }, :role => "guest", :requirements=>{:role => /[a-z]/}
         response.should redirect_to(admins_user_path(assigns[:user], :role => "guest"))
       end
     end
-    
+
     describe "unssuccessfully" do
       before do
         mock_user(:role_code= => "", :enabled= => "", :update_attributes => false)
         User.stub(:find).with('1').and_return(mock_user)
       end
-      
+
       it "should expose user" do
         put :update, :id => "1", :user => { :first_name => "Joe" }, :role => "guest", :requirements=>{:role => /[a-z]/}
         assigns[:user].should eq(mock_user)
       end
-      
+
       it "should render edit" do
         put :update, :id => "1", :user => { :first_name => "Joe" }, :role => "guest", :requirements=>{:role => /[a-z]/}
         response.should render_template(:edit)
       end
     end
   end
-  
+
   describe "DELETE destroy" do
     before do
       mock_user(:destroy => true, :role => 'guest')
       User.stub(:find).with('1').and_return(mock_user)
     end
-    
+
     it "should expose user" do
       delete :destroy, :id => '1', :role => "guest", :requirements=>{:role => /[a-z]/}
       assigns[:user].should eq(mock_user)
     end
-    
+
     it "should delete" do
       mock_user.should_receive(:destroy).and_return(true)
       delete :destroy, :id => '1', :role => "guest", :requirements=>{:role => /[a-z]/}
     end
-    
+
     it "should redirect" do
       delete :destroy, :id => '1', :role => "guest", :requirements=>{:role => /[a-z]/}
       response.should redirect_to(admins_users_path(:role => 'guest'))
--- spec/controllers/password_resets_controller_spec.rb
+++ spec/controllers/password_resets_controller_spec.rb
@@ -1,31 +1,31 @@
 require File.dirname(__FILE__) + "/../spec_helper"
 
 describe PasswordResetsController do
-  
+
   def mock_user(stubs={})
     @mock_user ||= mock_model(User, {:email => "test@test.com", :perishable_token => "abc123"}.merge(stubs))
   end
-  
+
   context "GET new" do
     it "should render the new template" do
       get :new
       response.should render_template(:new)
     end
   end
-  
+
   context "GET edit" do
     it "should find and expose @user" do
       User.should_receive(:find_using_perishable_token).with("abc123").and_return(mock_user)
       get :edit, :id => "abc123"
       assigns[:user] = mock_user
     end
-    
+
     it "should render the edit template" do
       User.stub(:find_using_perishable_token).and_return(mock_user)
       get :edit, :id => mock_user.perishable_token
       response.should render_template(:edit)
     end
-    
+
     context "without existing user" do
       it "should redirect to login path with a flash notice" do
         get :edit, :id => "abc"
@@ -34,7 +34,7 @@ describe PasswordResetsController do
       end
     end
   end
-  
+
   context "POST create" do
     context "successfuly" do
       it "should find and expose user" do
@@ -43,13 +43,13 @@ describe PasswordResetsController do
         post :create, :email => mock_user.email
         assigns[:user].should == mock_user
       end
-    
+
       it "should deliver password reset instructions" do
         User.stub(:find_by_email).with(mock_user.email).and_return(mock_user)
         mock_user.should_receive(:deliver_password_reset_instructions!)
         post :create, :email => mock_user.email
       end
-      
+
       it "should redirect to login path" do
         User.stub(:find_by_email).with(mock_user.email).and_return(mock_user)
         mock_user.stub(:deliver_password_reset_instructions! => true)
@@ -57,20 +57,20 @@ describe PasswordResetsController do
         response.should redirect_to(login_path)
       end
     end
-    
+
     context "UNsuccessfully" do
       it "should find and expose user as nil" do
         User.should_receive(:find_by_email).with(mock_user.email).and_return(nil)
         post :create, :email => mock_user.email
         assigns[:user].should be_nil
       end
-      
+
       it "should render the new action" do
         User.should_receive(:find_by_email).with(mock_user.email).and_return(nil)
         post :create, :email => mock_user.email
         response.should render_template(:new)
       end
-      
+
       it "should display a flash" do
         User.should_receive(:find_by_email).with(mock_user.email).and_return(nil)
         post :create, :email => mock_user.email
@@ -78,42 +78,42 @@ describe PasswordResetsController do
       end
     end
   end
-    
+
     context "update" do
       context "succesfully" do
         it "should expose user" do
-          mock_user(:save => true, :password= => 'bla', :password_confirmation= => 'bla', :role => :admin)
+          mock_user(:save => true, :password= => 'bla', :password_confirmation= => 'bla', :role => :admin, :active => true)
           User.stub(:find_using_perishable_token).with('37').and_return(mock_user)
           put :update, :id => "37", :user => {}
           assigns[:user].should eq(mock_user)
         end
-        
+
         it "should update the password" do
-          mock_user(:save => true, :password= => 'bla', :password_confirmation= => 'bla', :role => :admin)
+          mock_user(:save => true, :password= => 'bla', :password_confirmation= => 'bla', :role => :admin, :active => true)
           User.stub(:find_using_perishable_token).with('37').and_return(mock_user)
           mock_user.should_receive(:password=)
           mock_user.should_receive(:password_confirmation=)
           put :update, :id => "37", :user => {}
         end
-        
+
         it "should redirect accordingly" do
-          mock_user(:save => true, :password= => 'bla', :password_confirmation= => 'bla', :role => :admin)
+          mock_user(:save => true, :password= => 'bla', :password_confirmation= => 'bla', :role => :admin, :active => true)
           User.stub(:find_using_perishable_token).with('37').and_return(mock_user)
           put :update, :id => "37", :user => {}
           response.should redirect_to(admins_dashboard_path)
         end
       end
-      
+
       context "unsuccessfuly because it could not save" do
         it "should expose the user" do
-          mock_user(:save => false, :password= => 'bla', :password_confirmation= => 'bla')
+          mock_user(:save => false, :password= => 'bla', :password_confirmation= => 'bla', :active => true)
           User.stub(:find_using_perishable_token).with('37').and_return(mock_user)
           put :update, :id => "37", :user => {}
           assigns[:user].should eq(mock_user)
         end
-        
+
         it "should render the edit template and display a flash" do
-          mock_user(:save => false, :password= => 'bla', :password_confirmation= => 'bla')
+          mock_user(:save => false, :password= => 'bla', :password_confirmation= => 'bla', :active => true)
           User.stub(:find_using_perishable_token).with('37').and_return(mock_user)
           put :update, :id => "37", :user => {}
           flash[:notice].should_not be_empty
@@ -121,5 +121,5 @@ describe PasswordResetsController do
         end
       end
     end
-  
+
 end
